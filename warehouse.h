b37ec1e1c257a15805fb52fa75af906e1af0729d
Connor <connorotte@gmail.com>
2017-02-10 10:11:06 -0700

    work has begun

---
+++ warehouse.h
@@ -0,0 +1,12 @@
+#ifndef WAREHOUSE_H
+#define WAREHOUSE_H
+#include <string>
+#include <map>
+class warehouse
+{
+	private:
+		std::string name;
+		std::map<std::string, food> all_food;
+
+}
+#endif
